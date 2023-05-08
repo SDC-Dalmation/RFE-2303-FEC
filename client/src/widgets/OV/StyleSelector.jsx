93805a5bf6aa35199ae5dbce2921501407869f13
Koen Arnold <kostarnold@gmail.com>
2023-05-08 16:23:45 -0400

    Style Selector Skeleton fully in place

--- client/src/widgets/OV/StyleSelector.jsx
+++ client/src/widgets/OV/StyleSelector.jsx
@@ -2,29 +2,27 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Style from './Style.jsx';
 
-function StyleSelector({currentProduct, setAllProductStyles, allProductStyles}) {
+function StyleSelector({currentProduct, setCurrentStyle, setAllProductStyles, allProductStyles}) {
 
-  useEffect(()=>{axios.post('/productStyles', {product_id: currentProduct.id}).then((res)=>{setAllProductStyles(res.data.results)})},[])
 
-  const styleBtnHandler = function(e) {
-    e.preventDefault;
-    alert('clicked it');
+  // when the page renders, grab all the styles for this product and set the current style to be the first style in the list
+  useEffect(()=>{axios.post('/productStyles', {product_id: currentProduct.id}).then((res)=>{setAllProductStyles(res.data.results); setCurrentStyle(res.data.results[0])})},[])
+
+  const styleBtnHandler = function() {
+    return function(e) {
+      e.preventDefault;
+      alert('clicked it');
+    }
   }
 
-  if (currentProduct) {
-    return (
-      <div>
-        <div className="style-title">Styles</div>
-        {allProductStyles.map((style, index) => (
-          <Style style={style} key={index} styleBtnHandler={styleBtnHandler}/>
-        ))}
-      </div>
-    )
-  } else {
-      return (
-        <div>NO PRODUCT FOUND - STYLESELECTOR</div>
-      )
-  }
+  return (
+    <div>
+      <div className="style-title">Styles</div>
+      {allProductStyles.map((style, index) => (
+        <Style style={style} key={index} styleBtnHandler={styleBtnHandler}/>
+      ))}
+    </div>
+  )
 }
 
 export default StyleSelector;
