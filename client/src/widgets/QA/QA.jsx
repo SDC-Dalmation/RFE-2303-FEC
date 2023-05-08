58c6d3e064386a90266db4e9052152b82954d4f1
Daniel Leebron <danleebron@gmail.com>
2023-05-08 12:06:27 -0400

    built out questionlist component

--- client/src/widgets/QA/QA.jsx
+++ client/src/widgets/QA/QA.jsx
@@ -1,22 +1,23 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+import QuestionList from './QuestionList.jsx';
 
 function QA ({currentProduct}) {
 
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
 
-  useEffect(() => {axios.get(`/listQuestions/${37316}`)
+  useEffect(() => {axios.get(`/listQuestions/${currentProduct.id}`)
   .then((res) => {
     setQuestions(res.data.results);
   })
 }, []);
 
 if (questions.length > 0) {
+
   return(
     <div>
-      Questions and Answers
-      {questions[0].question_body}
+      <QuestionList questions={questions}/>
     </div>
   );
 }
---
+++ client/src/widgets/QA/QA.test.js
@@ -0,0 +1,6 @@
+import renderer from 'react-test-renderer';
+import QA from './QA';
+
+test('Ensuring that we can run tests', () => {
+  expect(2 + 2).toBe(4);
+});
