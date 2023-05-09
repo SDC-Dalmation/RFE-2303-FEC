9aa91e4f83db5d432eb05ed54fde8aea7f5cd3fd
Neeki Askari <neeki.askari@gmail.com>
2023-05-09 15:12:41 -0400

    function created to change date format to Month DD, YYYY

--- client/src/widgets/RR/ReviewTile.jsx
+++ client/src/widgets/RR/ReviewTile.jsx
@@ -3,13 +3,37 @@ import StarRatings from "react-star-ratings";
 
 function ReviewTile ({review}) {
   const [rating, setRating] = useState(review.rating);
+
+  const formatDate = (string) => {
+    let date = new Date(string);
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    let monthIndex = date.getMonth();
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    return `${months[monthIndex]} ${day}, ${year}`;
+  }
+
   return(
     <div className="tile">
-      <div>{review.rating}</div>
       <StarRatings rating={review.rating} starRatedColor="blue" numberOfStars={5} name="rating" starDimension="20px"
       starSpacing="1px"/>
-      <div>Date: {review.date}</div>
-      <p>Summary: {review.summary}</p>
+      <div>Date: {formatDate(review.date)}</div>
+      <p style={{"font-weight": "bold"}}>Summary: {review.summary}</p>
       <p>Body: {review.body}</p>
       <div>Recommend? {review.recommend}</div>
       <div>Name: {review.reviewer_name}</div>
