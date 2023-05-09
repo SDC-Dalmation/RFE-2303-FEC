3867379397c9cc5cbc7dc251d6c4e3a865608c46
Neeki Askari <neeki.askari@gmail.com>
2023-05-09 11:46:01 -0400

    More Reviews button working, need to make it scrollable

--- client/src/widgets/RR/RR.jsx
+++ client/src/widgets/RR/RR.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 import RatingBreakdown from "./RatingBreakdown.jsx"
 import ProductBreakdown from "./ProductBreakdown.jsx"
 import ReviewsList from "./ReviewsList.jsx"
 
-function RR ({ currentProduct }) {
+function RR ({currentProduct}) {
+
+
   return(
     <div className="RR">
       <h4>Ratings and Reviews</h4>
@@ -13,7 +16,7 @@ function RR ({ currentProduct }) {
           <ProductBreakdown />
         </div>
         <div className="Reviews-List">
-          <ReviewsList />
+          <ReviewsList currentProduct={currentProduct}/>
         </div>
       </div>
     </div>
