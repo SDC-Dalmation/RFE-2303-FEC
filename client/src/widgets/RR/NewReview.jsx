19561b677bbd9695d95bc2a934192a400584265f
Neeki Askari <neeki.askari@gmail.com>
2023-05-10 12:26:50 -0400

    Star rating selection and recommend radio buttons added and functional

--- client/src/widgets/RR/NewReview.jsx
+++ client/src/widgets/RR/NewReview.jsx
@@ -1,9 +1,43 @@
 import React, {useState, useEffect} from "react";
+import StarRatings from "react-star-ratings";
 
 function NewReview ({reviews, setReviews, setShowModal, currentProduct}) {
+  const [rating, setRating] = useState(0);
+  const [option, setOption] = useState("");
+  const [recommend, setRecommend] = useState(false);
+
+
+  const changeRating = (newRating, name) => {
+    setRating(newRating);
+  }
+
+  const handleOptionChange = (e) => {
+    e.preventDefault();
+    setOption(e.target.value);
+  }
+
+  useEffect(() => {
+    if (option === "Yes") {
+      setRecommend(true);
+    } else {
+      setRecommend(false);
+    }
+  }, [option])
 
   const handleClose = () => {
     setShowModal(false);
+  }
+
+  const handleSubmit = () => {
+    let form = e.target
+  }
+
+  const ratingText = {
+    1: "Poor",
+    2: "Fair",
+    3: "Average",
+    4: "Good",
+    5: "Great"
   }
 
   return(
@@ -14,9 +48,43 @@ function NewReview ({reviews, setReviews, setShowModal, currentProduct}) {
         <h5 className="modal-subtitle">{`About the ${currentProduct.name}`}</h5>
       </div>
       <div className="modal-body">
-        <form>
-          <div>Overall rating(stars)</div>
-          <div>Do you recommend this product? Yes or No</div>
+
+        <form onSubmit={handleSubmit}>
+
+          <div className="modal-rating">Overall rating
+          <StarRatings
+          rating={rating}
+          changeRating={changeRating}
+          starRatedColor="blue"
+          numberOfStars={5}
+          name="rating"
+          starDimension="20px"
+          starSpacing="1px"
+          />
+          <div style={{"marginLeft": "10px"}}>{ratingText[rating]}</div>
+          </div>
+
+          <div className="radio">Do you recommend this product?
+            <label>
+              <input
+                type="radio"
+                value="Yes"
+                checked={option==="Yes"}
+                onChange={handleOptionChange}
+              />
+              Yes
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="No"
+                checked={option==="No"}
+                onChange={handleOptionChange}
+              />
+              No
+            </label>
+          </div>
+
           <div>Characteristics</div>
           <input placeholder="enter summary"/>
           <input placeholder="enter review body"/>
@@ -24,9 +92,10 @@ function NewReview ({reviews, setReviews, setShowModal, currentProduct}) {
           <input placeholder="enter name"/>
           <input placeholder="enter email"/>
         </form>
+
       </div>
       <div className="modal-footer">
-        <button className="button" onClick={handleClose}>Submit Review</button>
+        <button className="button" type="submit" onClick={handleClose}>Submit Review</button>
       </div>
      </div>
     </div>
