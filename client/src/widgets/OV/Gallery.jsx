162caf39ae3b1d1bad6caebd456b14607e83a86f
Koen Arnold <kostarnold@gmail.com>
2023-05-10 10:22:24 -0400

    Added a photos folder to house all the photo components

--- client/src/widgets/OV/Gallery.jsx
+++ client/src/widgets/OV/Gallery.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ExtendedGallery from './ExtendedGallery.jsx';
-import GalleryPhoto from './GalleryPhoto.jsx';
-import MainGalleryPhoto from './MainGalleryPhoto.jsx';
+import GalleryPhoto from './Photos/GalleryPhoto.jsx';
+import MainGalleryPhoto from './Photos/MainGalleryPhoto.jsx';
 
 function Gallery({currentProduct, currentStyle, mainGalleryPic, setMainGalleryPic}) {
 
@@ -23,9 +23,9 @@ function Gallery({currentProduct, currentStyle, mainGalleryPic, setMainGalleryPi
     return (
       <div>
           <ExtendedGallery handleExtendedBtn={handleExtendedBtn} showModal={showModal}/>
-          <div style={{'display': 'flex', 'flex-direction': 'column', 'margin-right': '20px'}}>
+          <div style={{'display': 'flex', 'flexDirection': 'column', 'marginRight': '20px'}}>
             <MainGalleryPhoto setMainGalleryPic={setMainGalleryPic} mainGalleryPic={mainGalleryPic} currentStyle={currentStyle}/>
-              <div style={{'display': 'flex', 'justifyContent': 'center', 'align-self': 'flex-end'}}>
+              <div style={{'display': 'flex', 'justifyContent': 'center', 'alignSelf': 'flex-end'}}>
               {currentStyle.photos.map((photo, index) => (
                 <GalleryPhoto photo={photo} key={index} currentStyle={currentStyle} handlePicBtn={handlePicBtn}/>
               ))}
