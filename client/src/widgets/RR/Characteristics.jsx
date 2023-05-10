66a292433fa643af4e2689c90234ea25e639f16f
Neeki Askari <neeki.askari@gmail.com>
2023-05-10 15:11:25 -0400

    options state changed to use characteristiic id instead of name

--- client/src/widgets/RR/Characteristics.jsx
+++ client/src/widgets/RR/Characteristics.jsx
@@ -9,68 +9,42 @@ function Characteristics ({currentProduct}) {
   useEffect(() => {
     axios.post('/reviewMetadata', {product_id: currentProduct.id})
     .then((res) => {
-      setCharacteristics(Object.keys(res.data.characteristics));
+      let characteristicIds = {};
+      for (let characteristic in res.data.characteristics) {
+        characteristicIds[characteristic] = res.data.characteristics[characteristic].id;
+      }
+      setCharacteristics(characteristicIds);
     })
   }, [])
 
   const handleOptionChange = (e, characteristic) => {
+    const value = e.target.value;
     setOptions((prevState) => ({
       ...prevState,
-      [characteristic]: e.target.value,
+      [characteristic]: value,
     }));
-
   }
 
   return (
     <div className="characteristics-radio">Characteristics
-            {characteristics.map((characteristic, index) => {
+            {Object.keys(characteristics).map((characteristic, index) => {
+              const id = characteristics[characteristic];
               return (
-                <div key={index}>{characteristic}
-                <label>
-                <input
-                  type="radio"
-                  value="1"
-                  checked={options[characteristic]==="1"}
-                  onChange={(e) => handleOptionChange(e, characteristic)}
-                />
-                1
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="2"
-                  checked={options[characteristic]==="2"}
-                  onChange={(e) => handleOptionChange(e, characteristic)}
-                />
-                2
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="3"
-                  checked={options[characteristic]==="3"}
-                  onChange={(e) => handleOptionChange(e, characteristic)}
-                />
-                3
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="4"
-                  checked={options[characteristic]==="4"}
-                  onChange={(e) => handleOptionChange(e, characteristic)}
-                />
-                4
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="5"
-                  checked={options[characteristic]==="5"}
-                  onChange={(e) => handleOptionChange(e, characteristic)}
-                />
-                5
-              </label>
+                <div key={id}>{characteristic}
+                {[1, 2, 3, 4, 5].map((value) => {
+                  return (
+                    <label key={value}>
+                      <input
+                        type="radio"
+                        value={value}
+                        checked={options[id]===value.toString()}
+                        onChange={(e) => handleOptionChange(e, id)}
+                      />
+                      {value}
+                    </label>
+                  )
+                })}
+
                 </div>)
             })}
           </div>
