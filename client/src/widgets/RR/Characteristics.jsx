1352308a121049b7be683362a2b45c8fc96630a9
Neeki Askari <neeki.askari@gmail.com>
2023-05-10 16:31:05 -0400

    Characteristics section now meeting business requirements

--- client/src/widgets/RR/Characteristics.jsx
+++ client/src/widgets/RR/Characteristics.jsx
@@ -4,18 +4,24 @@ import axios from "axios";
 function Characteristics ({currentProduct}) {
   const [characteristics, setCharacteristics] = useState([]);
   const [options, setOptions] = useState({});
-
+  const [descriptions, setDescriptions] = useState({});
 
   useEffect(() => {
     axios.post('/reviewMetadata', {product_id: currentProduct.id})
     .then((res) => {
-      let characteristicIds = {};
-      for (let characteristic in res.data.characteristics) {
-        characteristicIds[characteristic] = res.data.characteristics[characteristic].id;
-      }
-      setCharacteristics(characteristicIds);
+      setCharacteristics(Object.keys(res.data.characteristics));
     })
   }, [])
+
+  //set initial descriptions to be none selected
+
+  useEffect(() => {
+    const initialDescriptions = {};
+    characteristics.forEach((characteristic) => {
+      initialDescriptions[characteristic] = "none selected"
+    });
+    setDescriptions(initialDescriptions)
+  }, [characteristics])
 
   const handleOptionChange = (e, characteristic) => {
     const value = e.target.value;
@@ -23,27 +29,59 @@ function Characteristics ({currentProduct}) {
       ...prevState,
       [characteristic]: value,
     }));
+
+    setDescription(characteristic, getDescription(characteristic, value));
+  }
+
+  const setDescription = (characteristic, description) => {
+    setDescriptions((prevState) => ({
+      ...prevState,
+      [characteristic]: description,
+    }));
+  };
+
+  const getDescription = (characteristic, value) => {
+    const tableDetails = {
+      Size: ["A size too small", "1/2 a size too small", "Perfect", "1/2 a size too big", "A size too wide"],
+      Width: ["Too narrow", "Slightly narrow", "Perfect", "Slightly wide", "Too wide"],
+      Comfort: ["Uncomfortable", "Slightly narrow", "Perfect", "Slightly wide", "Too wide"],
+      Quality: ["Too narrow", "Slightly uncomfortable", "Ok", "Comfortable", "Perfect"],
+      Length: ["Runs Short", "Runs slightly short", "Perfect", "Runs slightly long", "Runs long"],
+      Fit: ["Runs tight", "Runs slightly tight", "Perfect", "Runs slightly long", "Runs long"]
+    }
+    //return value ? tableDetails[characteristic][value - 1]: "none selected";
+    if (value) {
+      return tableDetails[characteristic][value - 1];
+    } else {
+      return "none selected"
+    }
   }
 
   return (
     <div className="characteristics-radio">Characteristics
-            {Object.keys(characteristics).map((characteristic, index) => {
-              const id = characteristics[characteristic];
+            {characteristics.map((characteristic, index) => {
               return (
-                <div key={id}>{characteristic}
-                {[1, 2, 3, 4, 5].map((value) => {
-                  return (
-                    <label key={value}>
-                      <input
-                        type="radio"
-                        value={value}
-                        checked={options[id]===value.toString()}
-                        onChange={(e) => handleOptionChange(e, id)}
-                      />
-                      {value}
-                    </label>
-                  )
-                })}
+                <div className="single-characteristic" key={index}>
+                  <h4 className="characteristic-name">{characteristic}</h4>
+                  <p>
+                    {descriptions[characteristic]}
+                  </p>
+
+                  <div>
+                  {[1, 2, 3, 4, 5].map((value) => {
+                    return (
+                      <label key={value}>
+                        <input
+                          type="radio"
+                          value={value}
+                          checked={options[characteristic]===value.toString()}
+                          onChange={(e) => handleOptionChange(e, characteristic)}
+                        />
+                        {value}
+                      </label>
+                    )
+                  })}
+                  </div>
 
                 </div>)
             })}
