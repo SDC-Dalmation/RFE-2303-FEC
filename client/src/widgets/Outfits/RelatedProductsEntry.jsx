f31f30808ed987670db329ce1cd784413cb92638
Evan Pun <evanpun21403@gmail.com>
2023-05-10 11:58:39 -0400

    Added comparision modal box to center of screen

--- client/src/widgets/Outfits/RelatedProductsEntry.jsx
+++ client/src/widgets/Outfits/RelatedProductsEntry.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios"
+import CompareModal from "./CompareModal.jsx"
+import { createPortal } from 'react-dom';
 
 const RelatedProductsEntry = ({product, setCurrentProduct}) => {
   const [productInfo, setProductInfo] = useState({});
   const [productStyle, setProductStyle] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     axios.post('/productInformation', {
@@ -32,11 +35,21 @@ const RelatedProductsEntry = ({product, setCurrentProduct}) => {
     })
   }
 
+  const openModal = () => {
+    setShowModal(!showModal);
+  };
+
   if (productStyle) {
     return (
       <div style={{
         border: '1px solid grey',
+        position: 'relative'
         }} onClick={changeProduct}>
+        <img onClick={openModal} style={{width: 20, height: 20, position: 'absolute', right: 0}} src={'https://t3.ftcdn.net/jpg/01/82/37/42/360_F_182374246_4f3x34AyBdCii4kGtK6s8PNT4AVYgHRa.jpg'}/>
+        {showModal &&
+          createPortal(<CompareModal showModal={showModal} setShowModal={setShowModal} product={product}/>,
+          document.getElementById("modal"))
+        }
         <img style={{width: 200, height: 200}} src={productStyle}/>
         <div>{productInfo.category}</div>
         <div>{productInfo.name}</div>
@@ -47,7 +60,13 @@ const RelatedProductsEntry = ({product, setCurrentProduct}) => {
     return (
       <div style={{
         border: '1px solid grey',
-        padding: 10}} onClick={changeProduct}>
+        position: 'relative'
+        }} onClick={changeProduct}>
+        <img onClick={openModal} style={{width: 20, height: 20, position: 'absolute', right: 0}} src={'https://t3.ftcdn.net/jpg/01/82/37/42/360_F_182374246_4f3x34AyBdCii4kGtK6s8PNT4AVYgHRa.jpg'}/>
+        {showModal &&
+          createPortal(<CompareModal showModal={showModal} setShowModal={setShowModal} product={product}/>,
+          document.getElementById("modal"))
+        }
         <img style={{width: 200, height: 200}} src={'https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png'}/>
         <div>{productInfo.category}</div>
         <div>{productInfo.name}</div>
