0878b56f687698d1cc418f06e35e5e0120fc41a7
Evan Pun <evanpun21403@gmail.com>
2023-05-11 12:24:23 -0400

    Added delete button for Outfits component and changed placeholder image

--- client/src/widgets/Outfits/YourOutfitsEntry.jsx
+++ client/src/widgets/Outfits/YourOutfitsEntry.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const YourOutfitsEntry = ({item}) => {
+const YourOutfitsEntry = ({item, items, setItems}) => {
   const [productStyle, setProductStyle] = useState({});
 
   useEffect(() => {
@@ -11,12 +11,22 @@ const YourOutfitsEntry = ({item}) => {
       .then((info) => {
         setProductStyle(info.data.results[0].photos[0].thumbnail_url);
     })
-  }, [])
+  })
+
+  const deleteOutfit = (item) => {
+    const filtered = items.filter((outfit) => {
+      return outfit.id !== item.id
+    })
+    setItems(filtered);
+  }
 
   return (
     <div style={{
+      position: 'relative',
       border: '1px solid grey',
       }}>
+      <button style={{position: 'absolute', right: 0, top: 0}}
+      onClick={() => deleteOutfit(item)}>X</button>
       <img style={{width: 200, height: 200}} src={productStyle}/>
       <div>{item.category}</div>
       <div>{item.name}</div>
