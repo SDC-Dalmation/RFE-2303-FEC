6082c91ecbeab0e44a8c578b851092f661d56923
Daniel Leebron <danleebron@gmail.com>
2023-05-15 19:53:18 -0400

    added tests

--- client/src/widgets/QA/QuestionList.test.js
+++ client/src/widgets/QA/QuestionList.test.js
@@ -3,38 +3,12 @@
  */
 
 import { render, screen, fireEvent, act } from '@testing-library/react';
-import App from '../../App';
+import userEvent from '@testing-library/user-event'
 import QuestionList from './QuestionList';
-import QuestionModal from './QuestionModal';
 import Question from './Question';
-import QA from './QA';
 import axios from 'axios';
 
-test('renders the landing page', () => {
-  render(<App />);
-});
-
-test('question modal shows fields and a close button', () => {
-
-  const handleClose = jest.fn();
-
-
-  const {getByText} = render(
-    <QuestionModal onClose={handleClose} productID={37311} />
-  )
-
-  expect(screen.getByText('Your Question:')).toBeTruthy();
-  expect(screen.getByText('Your Display Name:')).toBeTruthy();
-  expect(screen.getByText('For privacy reasons, do not use your full name or email address')).toBeTruthy();
-
-
-  fireEvent.click(getByText(/Close Out/i))
-
-
-  expect(handleClose).toHaveBeenCalledTimes(1)
-});
-
-test('QA component displays header', async () => {
+test('QuestionList expands when more questions are clicked', async () => {
 
   const product = {
     "id": 37311,
@@ -60,7 +34,7 @@ test('QA component displays header', async () => {
 
   const testQuestion = {data : {results : [{"question_id":645638,"question_body":"New question for you!!","question_date":"2023-03-29T00:00:00.000Z","asker_name":"DJK","question_helpfulness":13,"reported":false,"answers":{"5991354":{"id":5991354,"body":"New answer for you!","date":"2023-03-29T00:00:00.000Z","answerer_name":"DJK","helpfulness":2,"photos":["https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"]},"5991757":{"id":5991757,"body":"Will this product let me protect the krabby patty formula?","date":"2023-05-09T00:00:00.000Z","answerer_name":"Mr. Crabs","helpfulness":1,"photos":[]},"5991788":{"id":5991788,"body":"I have images","date":"2023-05-09T00:00:00.000Z","answerer_name":"imagemaster","helpfulness":0,"photos":[]},"5991789":{"id":5991789,"body":"I have answer","date":"2023-05-09T00:00:00.000Z","answerer_name":"asd","helpfulness":0,"photos":["C:\\fakepath\\bojack_background.jpg","C:\\fakepath\\RFE2302.png"]}}}]}};
 
-  const otherTestQuestion = {
+  const testQuestions = {
     data : {
       results : [{
         "question_id": 645638,
@@ -79,32 +53,85 @@ test('QA component displays header', async () => {
             "photos": [
               "https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"
             ]
-          },
-          "5991757": {
-            "id": 5991757,
-            "body": "Will this product let me protect the krabby patty formula?",
-            "date": "2023-05-09T00:00:00.000Z",
-            "answerer_name": "Mr. Crabs",
-            "helpfulness": 8,
-            "photos": []
-          },
-          "5991788": {
-            "id": 5991788,
-            "body": "I have images",
-            "date": "2023-05-09T00:00:00.000Z",
-            "answerer_name": "imagemaster",
-            "helpfulness": 1,
-            "photos": []
-          },
-          "5991789": {
-            "id": 5991789,
-            "body": "I have answer",
-            "date": "2023-05-09T00:00:00.000Z",
-            "answerer_name": "asd",
-            "helpfulness": 0,
+          }
+        }
+      },
+      {
+        "question_id": 6456386,
+        "question_body": "New question for you!!",
+        "question_date": "2023-03-29T00:00:00.000Z",
+        "asker_name": "DJK",
+        "question_helpfulness": 32,
+        "reported": false,
+        "answers": {
+          "5991354": {
+            "id": 59913546,
+            "body": "New answer for you!",
+            "date": "2023-03-29T00:00:00.000Z",
+            "answerer_name": "DJK",
+            "helpfulness": 46,
             "photos": [
-              "C:\\fakepath\\bojack_background.jpg",
-              "C:\\fakepath\\RFE2302.png"
+              "https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"
+            ]
+          }
+        }
+      },
+      {
+        "question_id": 6456385,
+        "question_body": "New question for you!!",
+        "question_date": "2023-03-29T00:00:00.000Z",
+        "asker_name": "DJK",
+        "question_helpfulness": 32,
+        "reported": false,
+        "answers": {
+          "5991354": {
+            "id": 59913545,
+            "body": "New answer for you!",
+            "date": "2023-03-29T00:00:00.000Z",
+            "answerer_name": "DJK",
+            "helpfulness": 46,
+            "photos": [
+              "https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"
+            ]
+          }
+        }
+      },
+      {
+        "question_id": 6456384,
+        "question_body": "New question for you!!",
+        "question_date": "2023-03-29T00:00:00.000Z",
+        "asker_name": "DJK",
+        "question_helpfulness": 32,
+        "reported": false,
+        "answers": {
+          "5991354": {
+            "id": 59913544,
+            "body": "New answer for you!",
+            "date": "2023-03-29T00:00:00.000Z",
+            "answerer_name": "DJK",
+            "helpfulness": 46,
+            "photos": [
+              "https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"
+            ]
+          }
+        }
+      },
+      {
+        "question_id": 6456333,
+        "question_body": "Haberdasher",
+        "question_date": "2023-03-29T00:00:00.000Z",
+        "asker_name": "DJK",
+        "question_helpfulness": 32,
+        "reported": false,
+        "answers": {
+          "5991354": {
+            "id": 59913543,
+            "body": "New answer for you!",
+            "date": "2023-03-29T00:00:00.000Z",
+            "answerer_name": "DJK",
+            "helpfulness": 46,
+            "photos": [
+              "https://res.cloudinary.com/dxbjpxspe/image/upload/v1680107782/benj/fcd6qeyiloxlknqhlrmg.png"
             ]
           }
         }
@@ -114,18 +141,26 @@ test('QA component displays header', async () => {
 
   jest.mock('axios');
 
-
-  const mock = jest.spyOn(axios, "get");
-  mock.mockImplementation(() => Promise.resolve(otherTestQuestion));
-
-  // const mockLocal = jest.spyOn(localStorage, "setItem");
-  // mockLocal.mockImplementation(return true);
+  const markHelpful = jest.fn();
 
 
-  await act( async () => render(<QA currentProduct={product} />));
+  const mock = jest.spyOn(axios, "post");
+  mock.mockImplementation(() => Promise.resolve());
 
-  // await(() => expect(screen.getByText('Questions & Answers')).toBeTruthy());
-  expect(screen.getByText('Questions & Answers')).toBeTruthy();
+
+  render(<QuestionList questions={testQuestions.data.results}
+    prodName={"Test Product"}
+    markHelpful={markHelpful}
+    helpfulQA={[]}
+    setHelpfulQA={markHelpful}
+    currentProduct={product}/>)
+
+
+  expect(screen.getByText('More Answered Questions')).toBeTruthy();
+
+  fireEvent.click(screen.getByText('More Answered Questions'));
+
+  expect(screen.getByText('Q: Haberdasher')).toBeDefined();
 
 
 });
