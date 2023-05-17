19ba79a9891709da9eb789a2bd6a37ba9e629ef7
Koen Arnold <kostarnold@gmail.com>
2023-05-17 19:15:49 -0400

    removed checkmarks from selected thumbnail

--- client/src/widgets/OV/Photos/ExtendedGalleryPhoto.jsx
+++ client/src/widgets/OV/Photos/ExtendedGalleryPhoto.jsx
@@ -5,16 +5,14 @@ function ExtendedGalleryPhoto({photo, currentStyle, handlePicBtn, mainGalleryPic
   if (currentlySelected === index) {
     return(
       <div className="extended-gallery-carousel-piece">
-        <p id="extended-thumbnail-checkmark">&#9989;</p>
-        <img src={photo.thumbnail_url} className="extended-gallery-thumbnail" style={{'border': '1px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
+        <img src={photo.thumbnail_url} className="extended-gallery-thumbnail" style={{'border': '2px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
       </div>
     )
   } else if (currentlySelected > currentStyle.photos.length) {
     if (index === 0) {
       return(
         <div className="extended-gallery-carousel-piece">
-          <p id="extended-thumbnail-checkmark">&#9989;</p>
-          <img src={photo.thumbnail_url} className="extended-gallery-thumbnail" style={{'border': '1px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
+          <img src={photo.thumbnail_url} className="extended-gallery-thumbnail" style={{'border': '2px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
         </div>
       )
     } else {
--- client/src/widgets/OV/Photos/GalleryPhoto.jsx
+++ client/src/widgets/OV/Photos/GalleryPhoto.jsx
@@ -5,16 +5,14 @@ function GalleryPhoto({photo, currentStyle, handlePicBtn, mainGalleryPic, setCur
   if (currentlySelected === index) {
     return(
       <div className="gallery-carousel-piece">
-        <p style={{'position': 'absolute'}}>&#9989;</p>
-        <img src={photo.thumbnail_url} className="gallery-thumbnail" style={{'border': '1px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
+        <img src={photo.thumbnail_url} className="gallery-thumbnail" style={{'border': '2px solid lightgreen'}} onClick={handlePicBtn(photo.url, index)}/>
       </div>
     )
   } else if (currentlySelected > currentStyle.photos.length) {
     if (index === 0) {
       return(
         <div className="gallery-carousel-piece">
-          <p style={{'position': 'absolute'}}>&#9989;</p>
-          <img src={photo.thumbnail_url} className="gallery-thumbnail" style={{'border': '1px solid green'}} onClick={handlePicBtn(photo.url, index)}/>
+          <img src={photo.thumbnail_url} className="gallery-thumbnail" style={{'border': '2px solid lightgreen'}} onClick={handlePicBtn(photo.url, index)}/>
         </div>
       )
     } else {
