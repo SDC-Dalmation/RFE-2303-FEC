fe58a597b35c8e20835476c5f6411fabf6fef7e4
Evan Pun <evanpun21403@gmail.com>
2023-05-18 16:11:12 -0400

    small spelling fix

--- client/src/widgets/Outfits/Outfit.jsx
+++ client/src/widgets/Outfits/Outfit.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import RelatedProductsCarousel from "./relatedProductsCarousel.jsx";
+import RelatedProductsCarousel from "./RelatedProductsCarousel.jsx";
 import YourOutfits from "./YourOutfits.jsx";
 
 
